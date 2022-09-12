1461ed4c7a0a16fd46ea3dcad61d6a4d3764f109
Masih <masih118shafiee@gmail.com>
2022-09-12 14:00:44 +0430

    jwt public and private keys command

--- docker/server/scripts/init-project.sh
+++ docker/server/scripts/init-project.sh
@@ -5,3 +5,6 @@ chown -R www-data:www-data public
 
 rm composer.lock
 composer install --prefer-dist --no-progress
+
+# To Generate JWT public and private keys
+php bin/console lexik:jwt:generate-keypair
